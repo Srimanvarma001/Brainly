3ac67a14939517524d840d046e5920a37cb6ef27
sriman varma <srimannarayanavarmadunde@gmail.com>
2025-05-05 14:53:16 +0530

    Converted FE and BE into regular folders

--- Brainly-Backend/dist/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGO_URL = exports.JWT_PASSWORD = void 0;
-exports.JWT_PASSWORD = "yoursecret";
-exports.MONGO_URL = "mongodb+srv://srimannarayanavarmadunde:sriman69@brainly.k90aodo.mongodb.net/";
--- Brainly-Backend/src/config.ts
+++
@@ -1,3 +0,0 @@
-export const JWT_PASSWORD = "yoursecret";
-export const MONGO_URL =
- "mongodb+srv://srimannarayanavarmadunde:sriman69@brainly.k90aodo.mongodb.net/";
